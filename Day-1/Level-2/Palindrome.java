c8c6a3633ce83ad78581d7cb34aa08a6b2ffb7ba
Aryan chaurasia <aryachaurasi20@gmail.com>
2025-01-17 11:25:31 +0530

    Added the palindrome code

--- Day-1/Level-2/Palindrome.java
+++ Day-1/Level-2/Palindrome.java
@@ -1,4 +1,40 @@
-package PACKAGE_NAME;
+import java.util.Scanner; //importing Scanner class
 
-public class Palindrome {
+
+
+public  class Palindrome{
+    private String text; // kept it private to maintain data security
+
+    // created a method to check if a string is palindrome or not
+    private boolean checkPalindrome() {
+        // iterating through the text to find if it is a palindrome or not
+        for (int i = 0; i < text.length() / 2; i++) {
+            if(text.charAt(i) != text.charAt(text.length() - i - 1)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    // created a method to display if a string is palindrome or not
+    public void isPalindrome(String text)
+    {
+        this.text = text;
+        if (checkPalindrome()) {
+            System.out.println(text + " is a palindrome");
+        } else {
+            System.out.println(text + " is not a palindrome");
+        }
+    }
+    public  static  void  main(String[] args){
+        Scanner sc = new Scanner(System.in); // object of scanner class
+        // created a string variable text and taking input for that
+        String text = sc.nextLine();
+
+        Palindrome pc = new Palindrome(); // object of PalindromeChecker class
+
+        // checking if input text is palindrome
+        pc.isPalindrome(text);
+
+    }
 }
