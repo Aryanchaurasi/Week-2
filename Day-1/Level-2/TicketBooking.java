48681919255bafa97d106c9a88038680c625a9a4
Aryan chaurasia <aryachaurasi20@gmail.com>
2025-01-17 11:28:53 +0530

    Added the program of ticket booking onto git

--- Day-1/Level-2/TicketBooking.java
+++ Day-1/Level-2/TicketBooking.java
@@ -1,4 +1,57 @@
-package PACKAGE_NAME;
+import java.util.Scanner; // imported scanner class
+
 
 public class TicketBooking {
+    private String movieName = "Demo"; // this will be defined by the admin
+    static final int noOfSeats = 100, pricePerSeat = 150; // this is predefined data
+    static int[] allocatedSeats = new int[noOfSeats];
+    int chosenSeat = 0;
+
+    // created a method to book the tickets
+    public boolean bookTicket(String movieName, int seatNumber) {
+        if(this.movieName.equals(movieName)) { // checking if movie name entered by user is same as which is defined by admin
+            // iterating through the array of allocatedSeats
+            for(int i = 0; i < noOfSeats; i++) {
+                if(seatNumber == allocatedSeats[i]) {
+                    return false; // since seat is already allocated
+                }
+            }
+            chosenSeat = seatNumber;
+            return true; // since seat is not allocated yet
+        }
+        return false;
+    }
+
+    // created a method to display the ticket
+    public void displayTicket(boolean isBooked) {
+        if(isBooked) {
+            System.out.println("The movie ticket is booked.");
+            System.out.println("Movie name: " + this.movieName);
+            System.out.println("Seat number: " + this.chosenSeat);
+            System.out.println("Total price: " + pricePerSeat);
+        }
+        else {
+            System.out.println("The movie ticket is not booked.");
+        }
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in); // object of scanner class
+
+        // created a variable to take input for movie name
+        System.out.println("Enter the movie name");
+        String movieName = sc.nextLine();
+
+        // taking the input for seatNumber
+        System.out.println("Enter the Seat number");
+        int seatNumber = sc.nextInt();
+
+        TicketBooking tb = new TicketBooking(); // object of MovieTicket class
+
+        // booking the ticket is input seat number is available for booking
+        boolean isBooked = tb.bookTicket(movieName, seatNumber);
+
+        // displaying the ticket if booked
+        mt.displayTicket(isBooked);
+
+    }
 }
